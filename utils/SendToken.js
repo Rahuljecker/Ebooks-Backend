691892d3e429cefd02bc3297fa442882ebccb372
RahulChatterjee1999 <112849417+RahulChatterjee1999@users.noreply.github.com>
2023-03-25 10:49:29 +0530

    SEND TOKEN 4

--- utils/SendToken.js
+++ utils/SendToken.js
@@ -3,7 +3,7 @@ export const SendToken=(res,user,message,statusCode=200)=>{
  const token=user.getJWTToken();
 
  const options={
-    expires:new Date(Date.now()+60*1000),
+    expires:new Date(Date.now()+5*60*1000),
     httpOnly:true,
     secure:true,
     sameSite:"none",
